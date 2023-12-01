2f89edbadb45c262e8a1ce63e2063dae0de08d39
Fernando Nathaniel Sutanto <67040382+nandonathaniel@users.noreply.github.com>
2023-12-01 15:39:11 +0700

    Update LCA.cpp

--- code/LCA.cpp
+++ code/LCA.cpp
@@ -1,67 +1,25 @@
-/* Quick Note :
- * Jangan Mikir Lama - lama, sampahin dulu aja kalo OI
- * Always Try to reset
-*/
-#include <bits/stdc++.h>
-using namespace std;
-#define ff first
-#define ss second
-#define pb push_back
-#define debug(val) cerr << "The value of " << #val << " is = " << val << '\n';
-typedef long double ld;
-typedef long long ll;
-typedef unsigned long long ull;
-const ld PI = 4*atan((ld)1);
-const ll mod = 1e9 + 7;
-const ll inf = 922337203685477;
-const ll nax = 2e5 + 5;
-
-ll t, n, q;
-ll a[nax];
-vector<ll> v[nax];
-ll dep[nax], pa[nax][35];
-
-bool cmp(pair<ll,ll> xx, pair<ll,ll> yy){
-	if(xx.ff == yy.ff){
-		return xx.ss > yy.ss;
-	}
-	return xx.ff < yy.ff;
-}
-
-void dfs_lca(ll idx, ll bfr, ll cur_dep = 0){
-	pa[idx][0] = bfr;
-	dep[idx] = cur_dep;
-	for(ll i = 1; i < 21; i++){
-		if(pa[idx][i-1] == -1) continue;
-		pa[idx][i] = pa[pa[idx][i-1]][i-1];
-	}
-	for(auto it : v[idx]){
-		if(it == bfr) continue;
-		dfs_lca(it, idx, cur_dep + 1);
+void dfs(int now,int par){
+	for(auto nxt : adj[now]){
+		if(nxt==par)continue;
+		pa[0][nxt]=now;
+		for(int i=1;i<LOG;i++)pa[i][nxt]=pa[i-1][pa[i-1][nxt]];
+		depth[nxt]=depth[now]+1;
+		dfs(nxt,now);
 	}
 }
 
-ll LCA(ll x, ll y){
-	if(dep[x] < dep[y]){
-		swap(x, y);
+int LCA(int u,int v){
+	if(depth[u]<depth[v])swap(u,v);
+	int diff=depth[u]-depth[v];
+	for(int i=LOG-1;i>=0;i--){
+		if(diff&(1<<i))u=pa[i][u];
 	}
-	ll diff = dep[x] - dep[y];
-	for(ll i = 0; i < 21; i++){
-		if((1 << i) & diff){
-			x = pa[x][i];
+	if(u==v)return u;
+	for(int i=LOG-1;i>=0;i--){
+		if(pa[i][u]!=pa[i][v]){
+			u=pa[i][u];
+			v=pa[i][v];
 		}
 	}
-	if(x == y) return x;
-	for(int i = 20; i >= 0; i--){
-		if(pa[x][i] != pa[y][i]){
-			x = pa[x][i];
-			y = pa[y][i];
-		}
-	}
-	return pa[x][0];
-}
-	
-int main(){ 
-	memset(pa, -1, sizeof(pa));
-	dfs_lca(1, -1);
+	return pa[0][u];
 }
